b9d35c0d3e30e47d751c04a60e6b7d03b88134f4
SergeyYakimov <teriertis@yandex.ru>
2019-11-26 02:42:02 +0800

    Made filter

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,6 +1,21 @@
 <template>
 	<div>
 		<h1>List</h1>
+
+		<div class="row">
+			<div class="input-field s6 col">
+				<select ref="select" v-model="filter">
+			      <option value="0" disabled selected>Choose your option</option>
+			      <option value="active">Active</option>
+			      <option value="outdated">Outdated</option>
+			      <option value="completed">Completed</option>
+			    </select>
+			    <label>Status filter</label>
+			</div>
+		</div>
+
+		<button v-if="filter" class="btn-small red" @click="clearFilter">Clear filter</button>
+
 		<hr>
 		<table v-if="tasks && tasks.length">
 			<thead>
@@ -14,7 +29,7 @@
 				</tr>
 			</thead>
 			<tbody>
-				<tr v-for="(task, idx) in tasks"
+				<tr v-for="(task, idx) in displayTasks"
 					:key="task.id"
 				>
 					<td>{{ idx + 1 }}</td>
@@ -41,9 +56,28 @@
 export default {
 
   name: 'List',
+  data: () => ({
+  	filter: null
+  }),
   computed: {
   	tasks() {
   		return this.$store.getters.tasks
+  	},
+  	displayTasks() {
+  		return this.tasks.filter(t => {
+  			if(!this.filter) {
+  				return true;
+  			}
+  			return t.status === this.filter;
+  		})
+  	}
+  },
+  mounted() {
+  	M.FormSelect.init(this.$refs.select);
+  },
+  methods: {
+  	clearFilter() {
+  		this.filter = null;
   	}
   }
 };
